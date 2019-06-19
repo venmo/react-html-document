f81e27406e454f70c8c25051a7d075ace0ec80ac
Jaben McCormack <4959492+princess@users.noreply.github.com>
2019-06-19 14:29:55 -0400

    Add babel build results

--- dist/HTMLDocument.js
+++ dist/HTMLDocument.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, '__esModule', {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
@@ -15,22 +17,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'd
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function _escapeDangerousTags(html) {
-  // Dangerous tags according to https://www.w3.org/TR/html52/semantics-scripting.html#restrictions-for-contents-of-script-elements
-  const dangerousTags = {
-    '<!--': '<\\!--',
-    '<script': '<\\script',
-    '</script': '<\\/script',
-    '<style': '<\\style',
-    '</style': '<\\/style'
-  };
-  let htmlString = String(html)
-  Object.entries(dangerousTags).forEach(([dangerousTag, replacementValue]) => {
-    htmlString = htmlString.replace(new RegExp(dangerousTag, 'g'), replacementValue);
-  });
-  return htmlString;
-}
 
 var _react = require('react');
 
@@ -45,6 +31,27 @@ var _constants = require('./constants');
 var _readFile = require('./readFile');
 
 var _readFile2 = _interopRequireDefault(_readFile);
+
+function _escapeDangerousTags(html) {
+  // Dangerous tags according to https://www.w3.org/TR/html52/semantics-scripting.html#restrictions-for-contents-of-script-elements
+  var dangerousTags = {
+    '<!--': '<\\!--',
+    '<script': '<\\script',
+    '</script': '<\\/script',
+    '<style': '<\\style',
+    '</style': '<\\/style'
+  };
+  var htmlString = String(html);
+  Object.entries(dangerousTags).forEach(function (_ref) {
+    var _ref2 = _slicedToArray(_ref, 2);
+
+    var dangerousTag = _ref2[0];
+    var replacementValue = _ref2[1];
+
+    htmlString = htmlString.replace(new RegExp(dangerousTag, 'g'), replacementValue);
+  });
+  return htmlString;
+}
 
 var HTMLDocument = (function (_Component) {
   _inherits(HTMLDocument, _Component);
